71c8a43b239e08f4091d19f246cf1df79ad43195
Garth Mortensen <mortensengarth@hotmail.com>
2024-04-22 19:53:01 -0500

    Refactor, write logs

--- entry.py
+++ entry.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv  # for loading env variables. pip install python-dotenv
-from pathlib import Path
+from pathlib import Path  # vs OS, this is OOP path handler
 import os
+import logging
+from datetime import datetime
 
+# import the necessary classes from langchain
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
@@ -14,40 +17,67 @@ from langchain.chat_models import ChatOpenAI
 # https://gist.github.com/defulmere/8b9695e415a44271061cc8e272f3c300
 # override sqlite 3.34 with newer version, per chromadb requirements
 # this is bc alma linux doesnt yet support sqlite3.35, only 3.34
-__import__('pysqlite3')
+__import__("pysqlite3")
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import openai
 
-# get openai key
+# logging setup
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 home = Path(__file__).resolve().parent
-load_dotenv(dotenv_path=home / '.env')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+log_filename = home / "logs" / f"{timestamp}.log"
+logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('testing')
 
-# find pdf file
-data_dir = home / 'data'
-data_file = '2024-stress-test-scenarios-20240215.pdf'
-data_file_path = data_dir / data_file
+# get openai key from uncommitted .env file
+load_dotenv(dotenv_path=home / ".env")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # insert coin to continue ($5)
 
+# load the documents from the PDF file and split them into chunks of 1000 characters with 0 character overlap
+# this avoids openais 4096 token limit
+data_file_path = home / "data" / "2024-stress-test-scenarios-20240215.pdf"
 loader = PyPDFLoader(data_file_path.absolute())
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0) # specify how many characters per chunk and overlap
-documents = loader.load()
-texts = text_splitter.split_documents(documents)
+text_splitter = CharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=0
+)  # specify how many characters per chunk and overlap
 
-# select which embeddings we want to use
-embeddings = OpenAIEmbeddings()
 
-# Create embeddings for each chunk and insert into the Chroma vector database.
-db = Chroma.from_documents(texts, embeddings)
+MODEL = "gpt-3.5-turbo"
+TEMPERATURE = 0  # temperature=0 means most deterministic
+def setup_qa_chain():
+    """Set up a question-answering chain
 
-# Create a language model and a retriever
-llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
-retriever = db.as_retriever()
+    returns:
+        RetrievalQA: the retrieval question-answering chain
+    """
+    documents = loader.load()
+    texts = text_splitter.split_documents(documents)
 
-# Create a QA chain
-chain = RetrievalQA.from_chain_type(llm, retriever=retriever, chain_type='stuff')
+    # select which embeddings we want to use
+    embeddings = OpenAIEmbeddings()
 
-query = "Describe this year's severely adverse scenario, please sir."
-query_response = chain.invoke(query)
-print(query_response['result'])
+    # create embeddings for each chunk and insert into the chroma vector database.
+    db = Chroma.from_documents(texts, embeddings)
 
+    # create a language model and a retriever
+    llm = ChatOpenAI(model=MODEL, temperature=TEMPERATURE)
+    retriever = db.as_retriever()
+
+    return RetrievalQA.from_chain_type(llm, retriever=retriever, chain_type="stuff")
+
+
+def ask_and_print(chain, question: str) -> None:
+    """Ask a question to a chain and print the response.
+
+    Args:
+        chain: The chain object to invoke the question on.
+        question (str): The question to ask.
+    """
+    response = chain.invoke(question)
+    logger.info(f"{question}\n{response['result']}\n\n")
+
+
+# create a QA chain
+chain = setup_qa_chain()
+ask_and_print(chain, "Explain in simple terms what stress testing is.")
+ask_and_print(chain, "Give me a short summary of how this years really bad scenarios differ from previous years.")
